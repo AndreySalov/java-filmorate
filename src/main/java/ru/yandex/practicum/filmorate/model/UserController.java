d0ba41a66493e055422d08fad892f10459847ec3
AndreySalov <andreysalov@yandex.ru>
2023-06-07 00:47:49 +0300

    ТЗ №9

--- src/main/java/ru/yandex/practicum/filmorate/model/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/UserController.java
@@ -2,16 +2,12 @@ package ru.yandex.practicum.filmorate.model;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import lombok.Data;
-import lombok.AllArgsConstructor;
 
 
 @RestController
