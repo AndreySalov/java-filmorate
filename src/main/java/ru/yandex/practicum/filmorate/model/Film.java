7a86f8fea1a287080ae38499583dfadf1e5f198c
AndreySalov <andreysalov@yandex.ru>
2023-06-07 00:33:43 +0300

    ТЗ №9

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,10 +1,11 @@
 package ru.yandex.practicum.filmorate.model;
 
 import java.time.LocalDate;
-import java.util.Date;
+
 import lombok.Data;
 import lombok.NonNull;
 import lombok.AllArgsConstructor;
+
 @Data
 public class Film {
     private int id;
