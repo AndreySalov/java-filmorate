723f889ef0c74a6f3cfee9614e8d1cba967ae15c
AndreySalov <andreysalov@yandex.ru>
2023-06-07 00:46:23 +0300

    ТЗ №9

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
--- src/main/java/ru/yandex/practicum/filmorate/model/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/UserController.java
@@ -16,36 +16,38 @@ import lombok.AllArgsConstructor;
 
 @RestController
 public class UserController {
-   private final Map<Integer,User> users = new HashMap<>();
-   private final UserValidation userValidation = new UserValidation();
+    private final Map<Integer, User> users = new HashMap<>();
+    private final UserValidation userValidation = new UserValidation();
+    private final Logger log = LoggerFactory.getLogger(UserController.class);
     private int currentId;
 
-    private final static Logger log = LoggerFactory.getLogger(UserController.class);
     @GetMapping("/users")
     public List<User> getAllUsers() {
         return new ArrayList<>(users.values());
     }
+
     @PostMapping(value = "/users")
     public User create(@RequestBody User user) {
         userValidation.valid(user);
-        int newId =getNewId();
+        int newId = getNewId();
         user.setId(newId);
-        users.put(newId,user);
+        users.put(newId, user);
         log.info("Добавлен пользователь. id=" + newId);
         return user;
     }
+
     @PutMapping(value = "/users")
     public User updateUser(@RequestBody User user) {
         if (users.containsKey(user.getId())) {
             userValidation.valid(user);
             users.put(user.getId(), user);
             log.info("Изменен пользователь c id=" + user.getId());
-        }
-        else
-            throw  new ValidationExeption("Пользователь с таким id не найден");
+        } else
+            throw new ValidationExeption("Пользователь с таким id не найден");
         return user;
     }
-    private int getNewId(){
+
+    private int getNewId() {
         return ++currentId;
     }
 }
