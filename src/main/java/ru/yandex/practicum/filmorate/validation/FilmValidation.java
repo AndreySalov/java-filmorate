a855ead72faf5002dbd35d8a5f361cddc0d74ad7
AndreySalov <andreysalov@yandex.ru>
2023-06-07 13:56:12 +0300

    ТЗ №9

--- src/main/java/ru/yandex/practicum/filmorate/validation/FilmValidation.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/FilmValidation.java
@@ -8,12 +8,11 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.time.LocalDate;
 
 public class FilmValidation {
-    final LocalDate MIN_DATE = LocalDate.of(1895, 12, 28);
-    final int MAX_DESCRIPTION = 200;
+    final LocalDate MINDATE = LocalDate.of(1895, 12, 28);
     private final Logger log = LoggerFactory.getLogger(FilmValidation.class);
 
     public void valid(Film film) throws ValidationException {
-        if (film.getReleaseDate().isBefore(MIN_DATE)) {
+        if (film.getReleaseDate().isBefore(MINDATE)) {
             log.info("Ошибка при проверка фильма. Неверная дата релиза");
             throw new ValidationException("Дата релиза — не раньше 28 декабря 1895 года");
         }
