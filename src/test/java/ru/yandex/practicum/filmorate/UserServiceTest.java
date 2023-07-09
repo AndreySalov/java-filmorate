cfd68d37b4b9afafb06c9f127ba0e014bee6946f
AndreySalov <andreysalov@yandex.ru>
2023-07-09 04:20:20 +0300

    Спринт 11_2

--- src/test/java/ru/yandex/practicum/filmorate/UserServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserServiceTest.java
@@ -2,15 +2,7 @@ package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
-import ru.yandex.practicum.filmorate.storage.UserDbStorage;
-
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserServiceTest {
     private UserService userService;
